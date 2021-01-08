11b9958749d96ce15c3a3864e6c7f426e119fe83
Benedict Bihlmaier git config user.namegit config --global user.name Benedict <Benedict.Bihlmaier@gmail.com>
2021-01-08 15:50:47 +0100

    work on cards feature

--- src/pages/[lang]/index.tsx
+++ src/pages/[lang]/index.tsx
@@ -5,6 +5,7 @@ import i18next from "i18next";
 import { getAllLanguageSlugs, getLanguage } from "../../lib/lang";
 import FlipCard from "../../components/sampleCardsFeature/FlipCard";
 import Link from "next/link";
+import { cards } from "../../util/cards";
 
 interface ILangIndex {
   language: string;
@@ -20,6 +21,10 @@ const LangIndex: NextPage<ILangIndex> = ({ language }) => {
     }
   };
   const [cardOnTop, setCardOnTop] = useState<number>(-1); //-1 for first render
+  const [cardNumber, setCardNumber] = useState("");
+  const [cardCategory, setCardCategory] = useState(
+    i18next.t("index.explanation")
+  );
 
   /**
    * Set initial pos & zIndex of every card & skip animation on first render.
@@ -31,8 +36,8 @@ const LangIndex: NextPage<ILangIndex> = ({ language }) => {
       cardRefs.current.map((card, index) => {
         cardRefs.current[index].style.zIndex =
           cardRefs.current.length - (index + 1);
-        cardRefs.current[index].style.top = 5 * index + "px";
-        cardRefs.current[index].style.left = 5 * index + "px";
+        cardRefs.current[index].style.top = 1 * index + "px";
+        cardRefs.current[index].style.left = 1 * index + "px";
       });
     } else {
       if (cardRefs.current.length > 0) {
@@ -40,17 +45,29 @@ const LangIndex: NextPage<ILangIndex> = ({ language }) => {
           card.style.animationName = "";
           const currentZIndex = parseInt(card.style.zIndex);
           const currentPos = parseInt(card.style.left); //used for left & top
-          const maxShift = (cardRefs.current.length - 1) * 5; //how much the last card should shift
+          const maxShift = (cardRefs.current.length - 1) * 1; //how much the last card should shift
           card.style.zIndex =
             currentZIndex === cardRefs.current.length - 1
               ? 0
               : currentZIndex + 1;
           card.style.left =
-            currentPos === 0 ? maxShift + "px" : currentPos - 5 + "px";
+            currentPos === 0 ? maxShift + "px" : currentPos - 1 + "px";
           card.style.top =
-            currentPos === 0 ? maxShift + "px" : currentPos - 5 + "px";
+            currentPos === 0 ? maxShift + "px" : currentPos - 1 + "px";
         });
         cardRefs.current[cardOnTop].style.animationName = "frontToBack";
+        if (cards[cardOnTop] === undefined) {
+          setCardNumber("");
+          setCardCategory(i18next.t("index.explanation"));
+        } else {
+          setCardNumber(`card ${cards[cardOnTop].number}/50`);
+          setCardCategory(
+            //@ts-ignore
+            i18next.t("index.category") +
+              " " +
+              i18next.t(cards[cardOnTop].category)
+          );
+        }
       }
     }
   }, [cardOnTop]);
@@ -62,65 +79,66 @@ const LangIndex: NextPage<ILangIndex> = ({ language }) => {
       description={i18next.t("siteMeta.descriptionIndex")}
     >
       <section className="w-full flex flex-col items-center h-screen">
-        <div className="w-full flex flex-col items-center justify-center md:pt-36 h-4/5 relative overflow-hidden">
-          <figure className="stack w-11/12 mx-auto md:w-1/4">
-            <div className="card one" ref={addToRefs}>
-              <FlipCard
-                imageFront={`/img/cards/InspirationWildCard_${language}_100.png`}
-                imageBack={`/img/cards/InspirationWildCard_${language}_101.png`}
-              />
+        <div className="w-11/12 h-screen">
+          <div className="w-full flex flex-col items-center justify-center md:pt-36 h-4/5 relative overflow-hidden">
+            <figure className="stack w-full mx-auto">
+              <div className="card" ref={addToRefs}>
+                <FlipCard
+                  wasFlipped={isFlipped => {
+                    if (isFlipped) {
+                      return true;
+                    } else {
+                      return false;
+                    }
+                  }}
+                  imageFront={`/img/cards/InspirationWildCard_de_000.png`}
+                  imageBack={`/img/cards/InspirationWildCard_de_001.png`}
+                />
+              </div>
+              {cards.map((card, index) => {
+                return (
+                  <div key={index} className="card" ref={addToRefs}>
+                    <FlipCard
+                      isFlippable={!card.imgBack.includes("na")}
+                      wasFlipped={isFlipped => {
+                        if (isFlipped) {
+                          return true;
+                        } else {
+                          return false;
+                        }
+                      }}
+                      imageFront={`/img/cards/InspirationWildCard_${language}_${card.imgFront}`}
+                      imageBack={`/img/cards/InspirationWildCard_${language}_${card.imgBack}`}
+                    />
+                  </div>
+                );
+              })}
+            </figure>
+            <div className="font-light text-secondary flex justify-between w-10/12 sm:w-1/2 lg:w-2/6 xl:w-1/4">
+              <p className="">{cardCategory}</p>
+              <p className="">{cardNumber}</p>
             </div>
-            <div className="card two" ref={addToRefs}>
-              <FlipCard
-                imageFront={`/img/cards/InspirationWildCard_${language}_200.png`}
-                imageBack={`/img/cards/InspirationWildCard_${language}_201.png`}
-              />
-            </div>
-            <div className="card three" ref={addToRefs}>
-              <FlipCard
-                imageFront={`/img/cards/InspirationWildCard_${language}_300.png`}
-                imageBack={`/img/cards/InspirationWildCard_${language}_303.png`}
-              />
-            </div>
-            <div className="card four" ref={addToRefs}>
-              <FlipCard
-                imageFront={`/img/cards/InspirationWildCard_${language}_400.png`}
-                imageBack={`/img/cards/InspirationWildCard_${language}_401.png`}
-              />
-            </div>
-            <div className="card five" ref={addToRefs}>
-              <FlipCard
-                imageFront={`/img/cards/InspirationWildCard_${language}_500.png`}
-                imageBack={`/img/cards/InspirationWildCard_${language}_501.png`}
-              />
-            </div>
-          </figure>
-          <p
-            style={{ left: "20px" }}
-            className="font-light mt-2 mx-auto w-10/12 md:w-1/4 relative"
-          >
-            {i18next.t("index.explanation")}
-          </p>
-        </div>
-        <div className="h-1/5 w-10/12 mx-auto md:w-1/4 flex items-start justify-between">
-          <Link prefetch={false} href={`/[lang]/buy`} as={`/${language}/buy`}>
+          </div>
+          <div className="h-1/5 mx-auto flex items-start justify-between sm:mt-6 w-10/12 sm:w-1/2 lg:w-2/6 xl:w-1/4 ">
+            <Link prefetch={false} href={`/[lang]/buy`} as={`/${language}/buy`}>
+              <button
+                type="button"
+                className="text-base md:text-xl xl:text-2xl font-light px-4 md:px-8 py-1 border-2 border-accent rounded-full hover:text-accent"
+              >
+                {i18next.t("index.buyBtn")}
+              </button>
+            </Link>
             <button
-              type="button"
-              className="text-base md:text-xl xl:text-2xl font-light mt-8 px-4 md:px-8 py-1 border-2 border-accent rounded-full hover:text-accent"
+              className="text-base md:text-xl xl:text-2xl font-semibold text-primary  px-4 md:px-8 py-1 border-2 border-accent rounded-full bg-accent hover:text-secondary"
+              onClick={() => {
+                cardOnTop === cardRefs.current.length - 1
+                  ? setCardOnTop(0)
+                  : setCardOnTop(cardOnTop + 1);
+              }}
             >
-              {i18next.t("index.buyBtn")}
+              {i18next.t("index.nextBtn")}
             </button>
-          </Link>
-          <button
-            className="text-base md:text-xl xl:text-2xl font-semibold text-primary mt-8 px-4 md:px-8 py-1 border-2 border-accent rounded-full bg-accent hover:text-secondary"
-            onClick={() =>
-              cardOnTop === cardRefs.current.length - 1
-                ? setCardOnTop(0)
-                : setCardOnTop(cardOnTop + 1)
-            }
-          >
-            {i18next.t("index.nextBtn")}
-          </button>
+          </div>
         </div>
       </section>
     </Layout>
---
+++ src/util/cards.ts
@@ -0,0 +1,97 @@
+export const cards = [
+  {
+    imgBack: "101.png",
+    imgFront: "100.png",
+    category: "index.cat_superforces",
+    number: 1
+  },
+  {
+    imgBack: "201.png",
+    imgFront: "200.png",
+    category: "index.cat_fictionlaws",
+    number: 11
+  },
+  {
+    imgBack: "303.png",
+    imgFront: "300.png",
+    category: "index.cat_superheroes",
+    number: 23
+  },
+  {
+    imgBack: "100na.png",
+    imgFront: "100na.png",
+    category: "index.cat_superforces",
+    number: 7
+  },
+  {
+    imgBack: "102.png",
+    imgFront: "100.png",
+    category: "index.cat_superforces",
+    number: 2
+  },
+  {
+    imgBack: "403.png",
+    imgFront: "400.png",
+    category: "index.cat_stereotypes",
+    number: 33
+  },
+  {
+    imgBack: "202.png",
+    imgFront: "200.png",
+    category: "index.cat_fictionlaws",
+    number: 12
+  },
+  {
+    imgBack: "300na.png",
+    imgFront: "300na.png",
+    category: "index.cat_superheroes",
+    number: 29
+  },
+  {
+    imgBack: "502.png",
+    imgFront: "500.png",
+    category: "index.cat_nightmares",
+    number: 42
+  },
+  {
+    imgBack: "306.png",
+    imgFront: "300.png",
+    category: "index.cat_superheroes",
+    number: 26
+  },
+
+  {
+    imgBack: "401.png",
+    imgFront: "400.png",
+    category: "index.cat_stereotypes",
+    number: 31
+  },
+  {
+    imgBack: "400na.png",
+    imgFront: "400na.png",
+    category: "index.cat_stereotypes",
+    number: 35
+  },
+  {
+    imgBack: "501.png",
+    imgFront: "500.png",
+    category: "index.cat_nightmares",
+    number: 41
+  }
+];
+
+export const cards_unavailable = [
+  {
+    imgBack: "200na.png",
+    imgFront: "200na.png",
+    category: "index.cat_fictionlaws",
+    number: 17
+  },
+
+  {
+    imgBack: "500na.png",
+    imgFront: "500na.png",
+    category: "index.cat_nightmares",
+    number: 47
+  }
+];
